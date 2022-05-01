00669d4f7e2913d23737feb001c1a4058e99e08b
Abhinav Nath <abhinavnath@ymail.com>
2022-05-01 12:48:35 +0530

    Create a multi-module gradle project

---
+++ config-management-with-spring-cloud-k8s/src/test/java/com/codecafe/springcloudk8s/ConfigManagementWithSpringCloudK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecafe.springcloudk8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigManagementWithSpringCloudK8sApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
